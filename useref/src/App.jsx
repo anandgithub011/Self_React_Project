edcd7b2c76e1e8ea4dd3dcd33ad5d7a1cfc48619
anandgithub011 <anandany830@gmail.com>
2024-06-15 00:47:59 +0530

    useref

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, SetData] = useState([]);
+
+  console.log(data);
+
+  const ref = useRef();
+  const handleClick = () => {
+    ref.current.focus();
+
+    let newData = {
+      title: ref.current.value,
+      id: Date.now(),
+    };
+    SetData([...data, newData]);
+    ref.current.value = "";
+
+    // console.log(ref.current.value);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input class="form-control" type="text" ref={ref} />
+        <button class="btn btn-success" onClick={handleClick}>
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((i) => (
+        <p key={i.id}>
+          {i.title} - {i.id}
+        </p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

b91f4c309619bd2456c61c045e1adfa366f44d7d
anandgithub011 <anandany830@gmail.com>
2024-06-15 17:24:19 +0530

    Password Eye View Changer

--- Password_Eye_function/src/App.jsx
+++ Password_Eye_function/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState(true);
+  const handleClick = () => {
+    setPassword(!password);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="d-inline-flex">
+        <input type={password ? "password" : "text"} placeholder="Password" />
+        <div>
+          {password ? (
+            <FaRegEye onClick={handleClick} />
+          ) : (
+            <FaRegEyeSlash onClick={handleClick} />
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

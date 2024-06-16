9b60b751012f5b1df0427dd9e294eb98511a0705
anandgithub011 <anandany830@gmail.com>
2024-06-16 10:57:46 +0530

    map higher order function

---
+++ Hof/index.js
@@ -0,0 +1,24 @@
+//i am talking about Higher Order Function.
+// 1. Map
+// 2. forEach
+// 3. filter
+// 4.reduce
+
+
+// 1. map
+
+// let arr = [1,2,3,4,5,55,6,77,888,67];
+let arr = ["Anand", "SumanSaurav","Amit"];
+
+
+let res = arr.map((el,index)=>{
+    return el;
+    
+})
+console.log(res);
+
+// tc : O(n);
+// Sc : O(1);
+
+
+

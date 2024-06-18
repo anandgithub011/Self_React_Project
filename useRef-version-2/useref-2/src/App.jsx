fe697b6a2404bd2214ddaadddc60a884c6814a9d
anandgithub011 <anandany830@gmail.com>
2024-06-18 22:42:01 +0530

    useref-1

--- useRef-version-2/useref-2/src/App.jsx
+++ useRef-version-2/useref-2/src/App.jsx
@@ -1,12 +1,66 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userData, setUserData] = useState([]);
 
-  return <></>;
+  const inputUserId = useRef();
+  const inputUserPass = useRef();
+
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+
+    // inputUserId.current.focus();
+
+    if (inputUserId.current.value == "" || inputUserPass.current.value == "") {
+      alert("Fill the UserId and Password");
+    } else {
+      let newData = [
+        ...userData,
+        {
+          id: Date.now(),
+          Username: inputUserId.current.value,
+          Password: inputUserPass.current.value,
+        },
+      ];
+      setUserData(newData);
+      inputUserId.current.value = "";
+      inputUserPass.current.value = "";
+    }
+  };
+
+  const handleDeleteClick = (id) => {
+    let newUserData = userData.filter((el) => el.id !== id);
+    setUserData(newUserData);
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmitClick}>
+          <input ref={inputUserId} type="text" />
+          <input ref={inputUserPass} type="text" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        {userData.map((i) => (
+          <div key={i.id} className="container border">
+            <div className="row">
+              <p className="col-4 modal-header">{`UserName: ${i.Username}`}</p>
+              <p className="col-4 modal-header">{`Password: ${i.Password}`}</p>
+              <button
+                className="btn btn-outline-danger col-2"
+                onClick={() => handleDeleteClick(i.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
